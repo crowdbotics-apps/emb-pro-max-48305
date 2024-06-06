0b11b18c86d1292af5cb93c00734490c60cb3689
Crowdbotics <team@crowdbotics.com>
2024-06-06 18:04:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "emb-pro-max-48305",
+  url: "https://emb-pro-max-48305.botics.co",
+  api: "https://emb-pro-max-48305.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
